9eae1b9590ca8805c6d577885f8295b322a93b4b
Shubham-moretask <shubham.kumar@moretasks.com>
2024-07-04 11:10:47 +0530

    new subscribe api make

--- index.js
+++ index.js
@@ -23,24 +23,56 @@ const publicVapidKey="BKfPlTWGsnyEIxx1FygtT-fOq4IG6d4FdMyqFl2aduJqZ5BQa_YMs3PkIS
 const privateVapidKey="IJkrCjxCNmbelKQ9MFdCIQXGv-oThPZ16_-1wM-NSus"
 webpush.setVapidDetails('mailto:sk9664150090@gmail.com', publicVapidKey, privateVapidKey);
 
-// Subscribe Route
-app.post('/subscribe', async (req, res) => {
-     // Save subscription to MongoDB
-     const { endpoint, keys } = req.body;
-     const subscriptionData = new Subscription({
-         endpoint,
-         keys
-     });
-     await subscriptionData.save();
 
-       console.log(req.body)
-  
-  const subscription = req.body;
-  res.status(201).json({});
-  const payload = JSON.stringify({ title: 'Push Test' });
-  webpush.sendNotification(subscription, payload)
-    .catch(err => console.error('Error sending push notification:', err));
+
+app.post('/subscribe', async (req, res) => {
+  // Save subscription to MongoDB
+  const { endpoint, keys } = req.body;
+
+  try {
+    // Check if a subscription with the given endpoint already exists
+    let subscriptionData = await Subscription.findOne({ endpoint });
+
+    if (!subscriptionData) {
+      // Create a new subscription if it does not exist
+      subscriptionData = new Subscription({ endpoint, keys });
+      await subscriptionData.save();
+    }
+
+    console.log(req.body);
+
+    const subscription = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify({ title: 'Push Test' });
+    webpush.sendNotification(subscription, payload)
+      .catch(err => console.error('Error sending push notification:', err));
+  } catch (error) {
+    console.error('Error handling subscription:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+
+
+
+// Subscribe Route
+// app.post('/subscribe', async (req, res) => {
+//      // Save subscription to MongoDB
+//      const { endpoint, keys } = req.body;
+
+//      const subscriptionData = new Subscription({
+//          endpoint,
+//          keys
+//      });
+//      await subscriptionData.save();
+
+//        console.log(req.body)
+  
+//   const subscription = req.body;
+//   res.status(201).json({});
+//   const payload = JSON.stringify({ title: 'Push Test' });
+//   webpush.sendNotification(subscription, payload)
+//     .catch(err => console.error('Error sending push notification:', err));
+// });
 
 // Start server
 app.listen(port, () => {
